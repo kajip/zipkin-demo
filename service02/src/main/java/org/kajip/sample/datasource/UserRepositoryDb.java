1a1c89bd092796c617f57df4ee7dc8e70bec16bf
Tomomi Kajita <tomomi.kajita@gmail.com>
2017-06-13 23:18:59 +0900

    適当なメソッドをSpanにする例を追加
    Signed-off-by: Tomomi Kajita <tomomi.kajita@gmail.com>

--- service02/src/main/java/org/kajip/sample/datasource/UserRepositoryDb.java
+++ service02/src/main/java/org/kajip/sample/datasource/UserRepositoryDb.java
@@ -4,6 +4,8 @@ import org.kajip.sample.domain.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +16,19 @@ public class UserRepositoryDb implements UserRepository {
     Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
+    Tracer tracer;
+
+    @Autowired
     JdbcTemplate  jdbcTemplate;
 
     @Override
+    @NewSpan("databaseAccess") // このメソッドを実行するとき、新しいSpanを開始する
     @Transactional
     public void regist() {
+
+        // Span Tag の追加。メソッドの引数に SpanTagアノテーションを渡す方法もある
+        tracer.addTag("sample-key", "sample-value");
+
         jdbcTemplate.query("SELECT 1", rs -> {
             logger.info("result: {}", rs.getInt(1));
         });
